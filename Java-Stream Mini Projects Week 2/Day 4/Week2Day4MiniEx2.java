d85dd4d13ef10a3bcb4e5e072068c9efae822bc6
Tristan Holloway <interncptwex036@capaciti.org.za>
2020-06-11 20:10:39 +0200

    Streamlined Version
    Newest Version

--- Java-Stream Mini Projects Week 2/Day 4/Week2Day4MiniEx2.java
+++ Java-Stream Mini Projects Week 2/Day 4/Week2Day4MiniEx2.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package week2day4miniex2;
-
 import java.util.Scanner;
 
-/**
- *
- * @author TristanH
- */
 public class Week2Day4MiniEx2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // calculate the sum of a numerical array
             
