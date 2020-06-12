e21c20968c659b88497534891356d99cc0a53b49
Tristan Holloway <interncptwex036@capaciti.org.za>
2020-06-12 08:16:10 +0200

    Updated

--- Java-Stream Mini Projects Week 2/Day 1/Exercise 2/Ex2.java
+++ Java-Stream Mini Projects Week 2/Day 1/Exercise 2/Ex2.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ex2;
-
 import java.util.Scanner;
 
-/**
- *
- * @author TristanH
- */
 public class Ex2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // determine max of 3 inputs
         
--- Java-Stream Mini Projects Week 2/Day 1/Exercise 3/Ex3.java
+++ Java-Stream Mini Projects Week 2/Day 1/Exercise 3/Ex3.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ex3;
-
 import java.util.Scanner;
 
-/**
- *
- * @author TristanH
- */
 public class Ex3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // receive a number between 1 and 7 and print out the corresponding day of the week
         // generate a random number if it is not given by the user
